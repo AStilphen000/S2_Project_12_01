9ab7e7e4a7cf1a18a6dbf392776473d397ceee93
Angelina Stilphen <astilphen000@west-mec.org>
2019-03-05 16:16:17 -0700

    Final Commit
    Ready for review

--- tc_cart.js
+++ tc_cart.js
@@ -21,12 +21,14 @@ var cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>
 // Created a for loop that loops through the entries in the item array to track the number of times a block of commands is run.
 for (var i = 0; i < item.length; i++) { 
 
-    // Created a variable named cartHTML, then used the += operator and added a string of 
+    // Created a variable named cartHTML, then used the += operator and added a string into the cartHTML varible. where description is the current value from the itemDescription array, price is the current value from the itemPrice array preceded by a $ symbol, and quantity is the current value from the itemQty array.
     cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='item' /></td>"
     cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
     itemCost = itemPrice[i] * itemQty[i];
     carttHTML +=  "<td>$" + itemCost + "</td>" + "</tr>";
     orderTotal = orderTotal + itemCost;
 }
+
+// where total is the value of the orderTotal variable, preceded by a $ symbol.
 cartHTML += "<tr>" + "<td colspan='4'>Subtotal</td>" + "<td>$" + orderTotal + "</td>" + "</tr>" + "</table>";
 document.getElementById("cart").innerHTML = cartHTML;
